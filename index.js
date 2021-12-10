32522db267d67fb2cfdbc68cac953945625792b9
Malcolm Cordova <mercadocreativo@gmail.com>
2021-12-10 12:13:00 -0400

    agregado: comando evitar perdida del get

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 const express = require('express');
 const { dbConnection } = require('./database/config');
 const cors = require('cors');
+const path = require('path');
 
 //crear server de express
 const app = express();
@@ -26,6 +27,11 @@ app.use('/api/medicos', require('./routes/medicos'));
 app.use('/api/login', require('./routes/auth'));
 app.use('/api/todo', require('./routes/busquedas'));
 app.use('/api/uploads', require('./routes/uploads'));
+
+//lo ultimo
+app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'public/index.html')); //ruta para produccion, evita perder la ruta
+});
 
 
 app.listen(process.env.PORT, () => {
